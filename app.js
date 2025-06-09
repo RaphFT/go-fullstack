bbecd1de7223cf13c77b234b86c635b04934c188
RaphFT <raph.frem@gmail.com>
2025-06-09 16:47:30 +0200

    secure app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+
 const userRoutes = require('./routes/user');
 
 const stuffRoutes = require('./routes/stuff');
